90c7bea17fe6d5ec1e6a30625206ebd649bcce56
Bastien MONTOIS <bastien.montois@gmail.com>
2024-09-02 23:17:59 +0200

    🐛 Fix Layout metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,7 +6,7 @@ import "./globals.css";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: `${siteConfig.name}`,
+  title: `${siteConfig.url} | For sale`,
   description: "This domain may be for sale, contact us for more information.",
 };
 
