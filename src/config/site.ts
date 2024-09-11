294ef2d2003a33a9de8f33045178a655503d8e66
Bastien MONTOIS <bastien.montois@gmail.com>
2024-09-11 21:30:26 +0200

    🐛 Fix NEXT_PUBLIC_URL

--- src/config/site.ts
+++ src/config/site.ts
@@ -1,5 +1,5 @@
 export const siteConfig = {
-  url: process.env.NEXT_PUBLIC_VERCEL_URL || "",
+  url: process.env.NEXT_PUBLIC_URL || "",
   emailFrom: process.env.EMAIL_FROM || "",
   emailTo: [process.env.EMAIL_TO || ""]
 }
